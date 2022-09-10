2abfb321c16771a8025cc437523016fcfce43b4a
sharifullah shirzad <sharifullahshirzad81@gmail.com>
2022-09-10 11:44:12 +0430

    my file shirzad

---
+++ javascript.js
@@ -0,0 +1,125 @@
+
+
+// const mysetofitems = new Set(['tv','pc','remot','pin'])
+
+// // mysetofitems.forEach(function(val){
+// //     console.log(val)
+// // }
+
+// mysetofitems.forEach((pa) =>{
+
+//     console.log(pa);
+
+// })
+
+
+
+// mysetofitems.add('chear');
+// mysetofitems.add('teble');
+// mysetofitems.add('khan');
+// mysetofitems.add('bangan');
+
+
+// mysetofitems.forEach((pa) =>{
+//     console.log(pa)
+
+// })
+
+// const itemsandprice = new Map([
+
+//     ["tv",500],
+//     ["pc",600],
+//     ["pin",20],
+// ]);
+// console.log(itemsandprice.get('tv'))
+// console.log(itemsandprice.get('pc'))
+// console.log(itemsandprice.get('pin'))
+
+// itemsandprice.set('car','bmw');
+
+// console.log(itemsandprice.get('car'))
+
+
+// itemsandprice.forEach ((key, val) =>{
+
+//     console.log(val +":"+ key)
+// })
+// itemsandprice.set('bicycle','japanesse')
+
+// for(const x of itemsandprice.entries()){
+//     console.log(x)
+// }
+// itemsandprice.set('Glass', 'Afghani')
+// console.log(itemsandprice.has('Glass'))
+
+
+
+// function myfun(){
+
+//     const themessge = document.getElementById('sms');
+//     themessge.innerHTML =` `;
+//     let x = document.getElementById('theinput').value;
+
+//     try{
+//         if(x == ` `) throw "Empty input box not allowed ";
+//         if(isNaN == (x)) throw "only Number is valid ";
+//         x = Number(x)
+//         if (x < 5) throw "it is small number "
+//         if (x < 10) throw "it is too big number "
+        
+//     } catch(error){
+//         themessge.innerHTML = error
+
+//     }
+
+//     finally {
+//         document.getElementById('theinput').value = ` `;
+//     }
+
+// }
+
+
+let x =1;
+var y = 5;
+const themessge = document.getElementById('sms');
+
+// try{
+
+//     // x.toPrecision(500)
+//     x = y +9
+//     console.log(x)
+// } catch (error){
+//     themessge.innerHTML= error.name
+
+// }
+
+
+// var k ='asdf'; 
+// try {
+//     k.toUppercase((k))
+
+// } catch(error){
+//     themessge.innerHTML = error.name
+// }
+
+// function myfunc(){
+//     const themessge = document.getElementById('sms');
+//     themessge.innerHTML =` `;
+//     let x = document.getElementById('input').value;
+
+//     try{
+//         if (x == ` `) throw "Empty cell not allowed"
+//         if(isNaN(x)) throw " only Number allowed"
+//         x= Number(x)
+//         if (x < 6) throw " this is less than our range "
+//         if (x < 12) throw "this is more than our range"
+//     } catch(error){
+//         themessge.innerHTML = errror
+//     }
+
+// }
+
+{
+    var x = 'shirzad'
+}
+var x ='shirzad'
